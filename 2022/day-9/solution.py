7b6be98d45bfa2acb5714f2d4796db729a05f3d1
nmgetahun <nmgetahun@gmail.com>
2023-06-11 12:46:17 -0400

    Day 9 complete

--- 2022/day-9/solution.py
+++ 2022/day-9/solution.py
@@ -7,8 +7,8 @@ Advent of Code Day 9 Challenge:
 https://adventofcode.com/2022/day/9
 """
 # ------------------------------------------------------------------------------
-import math
 
+ROPE_LEN = 10
 MOVES = {
     "R": (1, 0),
     "L": (-1, 0),
@@ -16,46 +16,69 @@ MOVES = {
     "D": (0, -1)
 }
 
-# Move head according to instruction and validate rope after each step
-def execute(instruction, head, tail, visited):
-    direction, count = MOVES[instruction[0]], instruction[1]
-    for _ in range(count):
-        head[0] += direction[0]
-        head[1] += direction[1]
-    
-    validate_rope(head, tail)
-    visited.add(tuple(tail))
 
+# Move head according to instruction and validate rope after each step
+def execute(direction, count, rope, visited1, visited2):
+    for _ in range(count):
+        rope[0][0] += direction[0]
+        rope[0][1] += direction[1]
+
+        for i, tail in enumerate(rope[1:]):
+            validate_rope(rope[i], tail)
+        
+        visited1.add((rope[1][0] - 100000, rope[1][1] - 100000))
+        visited2.add((rope[-1][0] - 100000, rope[-1][1] - 100000))
+        # print_state(head, tail)
+
+        
 # Ensure rope length = 1 unit (head, tail not too far apart)
 def validate_rope(head, tail):
     hx, hy = head[0], head[1]
     tx, ty = tail[0], tail[1]
     dx, dy = abs(hx - tx), abs(hy - ty)
 
-    if dx + dy > 1:
-        if dx + dy > 2:
-            x = (head[0] + tail[0]) / 2.0
-            y = (head[1] + tail[1]) / 2.0
-            tail[0] = math.ceil(x) if x >= 0 else math.floor(x)
-            tail[1] = math.ceil(y) if y >= 0 else math.floor(y)
-        elif hx == tx or hy == ty:
-            tail
-    elif dx + dy == 2
+    if dx + dy == 3:
+        if dx == 1:
+            tail[0] = head[0]
+        else:
+            tail[1] = head[1]
+
+    if dx == 2:
+        tail[0] = (head[0] + tail[0]) // 2
+    if dy == 2:
+        tail[1] = (head[1] + tail[1]) // 2
 
 
-def not_touching(head, tail):
-    
-    return dx > 1 or dy > 1 or dx + dy > 2
+# Test method for 5 x 6 board with sample input (for part 1)
+def print_state(head, tail):
+    head = [head[0] - 100000, head[1] - 100000]
+    tail = [tail[0] - 100000, tail[1] - 100000]
+    for y in list(range(5))[::-1]:
+        line = ""
+        for x in range(6):
+            if head[0] == x and head[1] == y:
+                line += "H "
+            elif tail[0] == x and tail[1] == y:
+                line += "T "
+            else:
+                line += ". "
+        print(line)
+    print("\n")
+
 
 # Main
 if __name__ == "__main__":
-    head, tail = [0, 0], [0, 0]
-    visited = {(0, 0)}
+    rope = [[100000, 100000] for _ in range(ROPE_LEN)]
+    visited1 = {(0, 0)}
+    visited2 = {(0, 0)}
 
     with open("input.txt") as file:
         for line in file:
             line = line.split()
-            execute((line[0], int(line[1])), head, tail, visited)
+            direction = MOVES[line[0]]
+            count = int(line[1])
+            execute(direction, count, rope, visited1, visited2)
     
-    print("Part 1:", len(visited))
-    print("Part 2:")
+    print("Part 1:", len(visited1))
+    print("Part 2:", len(visited2))
+
